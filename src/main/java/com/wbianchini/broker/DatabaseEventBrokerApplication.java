04c79f8a3e7a2ced2707885f06aba51f5f7cc76d
William Bianchini <william.bianchini@gmail.com>
2021-02-11 17:23:08 -0300

    initial commit

---
+++ src/main/java/com/wbianchini/broker/DatabaseEventBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.wbianchini.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseEventBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseEventBrokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wbianchini/broker/DatabaseEventBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbianchini.broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseEventBrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
